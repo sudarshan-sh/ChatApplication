3bacc87942a7297b1d98175f3a7299c5492c59d7
Sudarshan Sharma <mail2sudarshan.sharma@gmail.com>
2022-10-01 02:49:56 +0530

    UI enhancement

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import chatImg from '../images/chat-icon.png'
 
 const Home = ({socket}) => {
   const navigate = useNavigate();
@@ -13,18 +14,20 @@ const Home = ({socket}) => {
   };
   return (
     <form className="home__container" onSubmit={handleSubmit}>
-      <h2 className="home__header">Sign in to Open Chat</h2>
+        <img src={chatImg} alt="chatImg" className='home__Img' />
+      <h2 className="home__header">OPEN CHAT .....</h2>
       <label htmlFor="username">Username</label>
       <input
         type="text"
         minLength={6}
         name="username"
         id="username"
+        placeholder='Enter your username'
         className="username__input"
         value={userName}
         onChange={(e) => setUserName(e.target.value)}
       />
-      <button className="home__cta">SIGN IN</button>
+      <button className="home__cta">LOGIN</button>
     </form>
   );
 };
